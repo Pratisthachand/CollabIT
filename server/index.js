7c2601d7844a9318de53b70b3a597e6b893ab5f9
Pratisthachand <pratisthachand22@gmail.com>
2024-11-05 02:24:42 -0600

    Added controllers

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
+import { errorHandler, routeNotFound } from "./middlewares/errorMiddlewares.js";
+import routes from "./routes/index.js";
 import { dbConnection } from "./utils/index.js";
 
 dotenv.config();
@@ -29,7 +31,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
+app.use("/api", routes);
 
-// app.use("/api", routes);
+app.use(routeNotFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
