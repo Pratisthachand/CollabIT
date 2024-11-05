d496227b7d86ebc8a5cd0d330d892ab90c344e55
Pratisthachand <pratisthachand22@gmail.com>
2024-11-04 19:58:55 -0600

    Created react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,34 @@ import Tasks from "./pages/tasks";
 import Trash from "./pages/trash";
 import Users from "./pages/users";
 import Dashboard from "./pages/dashboard";
+import Taskdetails from "./pages/taskdetails";
+
+function Layout() {
+  const { user } = useSelector((state) => state.auth);
+
+  const location = useLocation();
+
+  return user ? (
+    <div className="w-full h-screen flex flex-col md:flex-row">
+      <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
+        {/* <Sidebar /> */}
+      </div>
+
+      {/* <MobileSidebar /> */}
+
+      <div className="flex-1 overflow-y-auto">
+        {/* <Navbar /> */}
+
+        <div className="p-4 2xl:px-10">
+          {/* <Outlet /> */}
+        </div>
+        
+      </div>
+    </div>
+  ) : (
+    <Navigate to="/log-in" state={{ from: location }} replace />
+  );
+}
 
 function App() {
   return (
@@ -20,7 +48,7 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/users" element={<Users />} />
           <Route path="/trash" element={<Trash />} />
-          <Route path="/task/:id" element={<Tasks />} />
+          <Route path="/task/:id" element={<Taskdetails />} />
         </Route>
 
         <Route path="/login" element={<Login />} />
